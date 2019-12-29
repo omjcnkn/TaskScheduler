64a65f18181009a2f7a9132bfed5ed54bee3eb30
Mohamed Hussein <mohamed.hussein.1.9.9.71.9.9.8@gmail.com>
2019-12-29 22:38:56 +0200

    added context menu

--- app/src/main/java/com/example/taskschedulerproject/MainActivity.java
+++ app/src/main/java/com/example/taskschedulerproject/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.taskschedulerproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -11,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -44,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             R.drawable.b5));
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +58,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
+        ShortcutInfo shortcut1 = new ShortcutInfo.Builder(this, "id1")
+                .setShortLabel("Home")
+                .setLongLabel("List of Tasks")
+                .setIcon(Icon.createWithResource(this, R.drawable.ic_home_black_24dp))
+                .setIntent(new Intent(MainActivity.this, MainActivity.class).setAction(Intent.ACTION_VIEW))
+                .build();
 
+        shortcutManager.setDynamicShortcuts(Arrays.asList(shortcut1));
 
         userBoard = UserBoard.getUserBoard(getApplicationContext());
         userBoard.subscribe(this);
